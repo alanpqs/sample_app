8fc26646726d47651afcc1e347d3c957da6cba40
Alan Miles <alan_miles@ajobwelldone.info>
2010-09-30 11:20:27 +0100

    Done with user edit/update, index and destroy actions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,6 +26,18 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   
   def test_sign_in(user)
-    controller.current_user = user
+    #controller.current_user = user
+    controller.sign_in(user)
+  end
+  
+  def integration_sign_in(user)
+    visit signin_path
+    fill_in :email,         :with => user.email
+    fill_in :password,      :with => user.password
+    click_button
+  end
+  
+  def gravatar_url
+    "http://gravatar.com/emails"
   end
 end
